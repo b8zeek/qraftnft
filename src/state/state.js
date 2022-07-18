1cdd5a97674e58eb6982cc6077b8da67f6d4d4a7
Mirko Basic <bejzik8@gmail.com>
2022-07-18 14:14:13 +0200

    Updated state.js.

--- src/state/state.js
+++ src/state/state.js
@@ -1,7 +1,10 @@
 import create from 'zustand'
 
 export const store = create(set => ({
-    wallet: null
+    phantomWalletInstalled: false,
+    phantomWallet: null,
+    setPhantomWalletInstalled: phantomWalletInstalled => set({ phantomWalletInstalled }),
+    setPhantomWallet: phantomWallet => set({ phantomWallet })
 }))
 
 export default store
