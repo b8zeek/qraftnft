1e984cdf290bb6a6fb98b950c5ab8f728bf055c4
Mirko Basic <bejzik8@gmail.com>
2022-07-19 12:38:24 +0200

    Updated landing page.

--- src/components/Robohash.jsx
+++ src/components/Robohash.jsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components'
 
+import { motion } from 'framer-motion'
+
 import Robohash from 'react-robohash'
 
 const RobohashComponent = ({ walletAddress }) =>
-    <RobohashContainer id='robohash-container'>
+    <RobohashContainer id='robohash-container'
+        
+    >
         <Robohash name={walletAddress} type='robot' />
     </RobohashContainer>
 
-const RobohashContainer = styled.div`
+const RobohashContainer = styled(motion.div)`
     text-align: center;
 `
 
--- src/pages/web/AboutPage.jsx
+++ src/pages/web/AboutPage.jsx
@@ -3,18 +3,10 @@ import styled from 'styled-components'
 import AnimatedPage from './AnimatedPage'
 import Heading from '../../components/Heading'
 import Text from '../../components/Text'
-import QRCode from '../../components/QRCode'
-
-import { useWallet } from '../../services/useWallet'
 
 const AboutPage = () => {
-    const { createConnection, getNftTokenData } = useWallet()
-    
     return <AnimatedPage>
-        <div>123</div>
-        <QRCode text='123' robohashURL='' />
-        <button onClick={createConnection}>Connect</button>
-        <button onClick={getNftTokenData}>Get NFTs</button>
+        
     </AnimatedPage>
 }
 
