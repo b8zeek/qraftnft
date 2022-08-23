e298851541d1613a1af33b4bf079a5b900b297c9
Basic Mirko <bejzik8@gmail.com>
2022-08-23 19:53:06 +0100

    Updated AnimatedSection component.

--- src/components/AnimatedSection.jsx
+++ src/components/AnimatedSection.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion'
 
-const AnimatedSection = ({ children, delay = 0 }) => (
-    <motion.div initial={{ scale: 0 }} animate={{ scale: 1, transition: { delay } }}>
+const AnimatedSection = ({ children, delay = 0, initial = { scale: 0 }, animate = { scale: 1 } }) => (
+    <motion.div initial={{ ...initial }} animate={{ ...animate, transition: { delay } }}>
         {children}
     </motion.div>
 )
